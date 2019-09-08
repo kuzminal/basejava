98ba4c7fe5b8106cd0f48ad1ee865ac0d5185734
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2019-09-08 21:34:31 +0300

    Структуру Storage не менял Реализовал все методы и протестировал на указанных классах

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,58 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int size = size();
+        for (int i=0; i < size; i++) {
+            if (storage[i] != null) storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (get(r.uuid) == null ) //проверят нет ли уже записи с таким же UUID
+            storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = null;
+        int size = size();
+        for (int i=0; i < size; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid))
+                result = storage[i];
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        int size = size();
+        for (int i=0; i < size; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                for (int j = i + 1; j < size; j++) { // добавил смещение элементов чтобы заполнить пустоту
+                    if (storage[j] != null) {                  // все элементы хранятся вначале массива
+                        storage[j-1] = storage[j];
+                        storage[j] = null;
+                    }
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] listResume = new Resume[size()];
+        for (int i=0; i < size(); i++) {
+            if (storage[i] != null) listResume[i] = storage[i];
+        }
+        return listResume;
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (int i=0; i < storage.length; i++) {
+            if (storage[i] != null) size++;
+        }
+        return size;
     }
 }
