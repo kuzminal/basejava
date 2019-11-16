ac0f730755908830ecce976e01322449f1faf77f
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2019-11-16 22:28:50 +0300

    HW9 Добавил отступы к файлам и каталогам и отсортировал по имени

--- src/com/kuzmin/FileList.java
+++ src/com/kuzmin/FileList.java
@@ -1,8 +1,12 @@
 package com.kuzmin;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class FileList {
+    private static String tab = "";
     public static void main(String[] args) {
         File file = new File("./src");
         printFilesFromDirectory(file);
@@ -10,11 +14,15 @@ public class FileList {
 
     private static void printFilesFromDirectory(File file) {
         if (file.listFiles() != null) {
-            for (File f : file.listFiles()) {
+            List<File> fileList = Arrays.asList(file.listFiles());
+            fileList.sort(Comparator.naturalOrder());
+            for (File f : fileList) {
                 if (f.isDirectory()) {
+                    System.out.println(tab + f.getName());
+                    tab += "\t";
                     printFilesFromDirectory(f);
                 } else {
-                    System.out.println(f.getName());
+                    System.out.println(tab + f.getName());
                 }
             }
         }
