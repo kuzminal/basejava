3d37908bdb4572487d071c1610345c70894eef4d
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2019-12-13 11:13:18 +0300

    HW11 В классе DeadLock создал метод lock(Object, Object) В него передаю в двух потоках объекты блокировки в разном порядке Отдельные классы с потоками не создаю, использую Thread и лямбды

--- src/DeadLock.java
+++ src/DeadLock.java
@@ -4,43 +4,28 @@ public class DeadLock {
 
 
     public static void main(String[] args) {
-        DeadThreadOne threadOne = new DeadThreadOne();
-        DeadThreadTwo threadTwo = new DeadThreadTwo();
+        Thread threadOne = new Thread(() -> {
+            lock(Lock1, Lock2);
+        });
+        Thread threadTwo = new Thread(() -> {
+            lock(Lock2, Lock1);
+        });
 
         threadOne.start();
         threadTwo.start();
     }
 
-    private static class DeadThreadOne extends Thread {
-        public void run() {
-            synchronized (Lock1) {
-                System.out.println("DeadThreadOne захватил объект Lock1");
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("DeadThreadOne ждет разблокировки Lock2");
-                synchronized (Lock2) {
-                    System.out.println("DeadThreadOne захватил объекты Lock1 и Lock2");
-                }
+    private static void lock(Object firstObjectForLock, Object secondObjectForLock){
+        synchronized (firstObjectForLock) {
+            System.out.println(Thread.currentThread().getName() + " захватил объект Lock2");
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }
-    }
-
-    private static class DeadThreadTwo extends Thread {
-        public void run() {
-            synchronized (Lock2) {
-                System.out.println("DeadThreadTwo захватил объект Lock2");
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("DeadThreadOne ждет освобождения Lock1");
-                synchronized (Lock1) {
-                    System.out.println("DeadThreadOne захватил объекты Lock1 и Lock2");
-                }
+            System.out.println(Thread.currentThread().getName() + " ждет освобождения Lock1");
+            synchronized (secondObjectForLock) {
+                System.out.println(Thread.currentThread().getName() + " захватил объекты Lock1 и Lock2");
             }
         }
     }
