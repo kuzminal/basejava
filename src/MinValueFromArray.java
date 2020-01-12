5dd088495f0f41954f88b20b78ed9501dbf7cbc8
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2020-01-12 22:04:15 +0300

    HW12 Переделал поиск минимума

--- src/MinValueFromArray.java
+++ src/MinValueFromArray.java
@@ -1,15 +1,14 @@
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MinValueFromArray {
     public static void main(String[] args) {
-        int[] arr = new int[]{6, 3, 9, 4, 4, 9, 1};
-        List<Integer> listValue = Arrays.stream(arr)
+        int[] arr = {6, 3, 1, 7, 4, 6, 7, 1};
+        List<Integer> listValue = IntStream.of(arr)
                 .boxed()
                 .collect(Collectors.toList());
-        int val = minValue(arr);
-        System.out.println(val);
+        System.out.println(minValue(arr));
         List<Integer> oddOrEven = oddOrEven(listValue);
         for (Integer ineg : oddOrEven) {
             System.out.println(ineg);
@@ -17,32 +16,18 @@ public class MinValueFromArray {
     }
 
     public static int minValue(int[] values) {
-        List<Integer> listValue = Arrays.stream(values)
+        return IntStream.of(values)
                 .boxed()
                 .distinct()
                 .sorted()
-                .collect(Collectors.toList());
-        int count = listValue.size();
-        int num = 0;
-        for (int i = 0; i < listValue.size() - 1; i++){
-            num += Math.pow(10, listValue.size() - (1 + i)) * listValue.get(i);
-        }
-        num += listValue.get(count -1);
-        return num;
+                .reduce(0, (a, b) -> a * 10 + b);
     }
 
     public static List<Integer> oddOrEven(List<Integer> integers) {
         int sum = integers.stream()
                 .reduce(0, Integer::sum);
-        if (sum % 2 == 0) {
-            return integers.stream()
-                    .filter(i -> i % 2 == 0)
-                    .collect(Collectors.toList());
-        } else {
-            return integers.stream()
-                    .filter(i -> i % 2 != 0)
-                    .collect(Collectors.toList());
-        }
-
+        return integers.stream()
+                .filter(i -> (sum % 2 != 0) == (i % 2 != 0))
+                .collect(Collectors.toList());
     }
 }
